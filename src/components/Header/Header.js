aea303fe211fecc97d10e9e558f70ff74b33d35f
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-22 17:01:17 -0300

    mobile menu effects

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 export const Header = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClickMenu = () => setClick(!click);
+
   return (
     <header>
       <div className='container'>
-        <div className='inner-content'>
-          <div className='brand'>
-            <Link to='/'>BestFilmes</Link>
-          </div>
-          <ul className='nav-links'>
+        <div className='brand'>
+          <Link to='/'>BestFilmes</Link>
+        </div>
+        <div onClick={handleClickMenu} className='inner-content'>
+          {click ? (
+            <i className='fas fa-times'></i>
+          ) : (
+            <i className='fas fa-bars'></i>
+          )}
+        </div>
+        <nav className='nav-wrapper'>
+          <ul className={click ? 'nav-links active' : 'nav-links'}>
             <li>
               <Link to='mylist'>Minha lista</Link>
             </li>
@@ -21,7 +32,7 @@ export const Header = () => {
               <Link to='favorites'>Favoritos</Link>
             </li>
           </ul>
-        </div>
+        </nav>
       </div>
     </header>
   );
