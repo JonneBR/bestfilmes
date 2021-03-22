2df097a6be6e747eb39189156b497afb2f18262c
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-22 15:57:45 -0300

    navbar menu css style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 export const Header = () => {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <header>
+      <div className='container'>
+        <div className='inner-content'>
+          <div className='brand'>
+            <Link to='/'>BestFilmes</Link>
+          </div>
+          <ul className='nav-links'>
+            <li>
+              <Link to='mylist'>Minha lista</Link>
+            </li>
+            <li>
+              <Link to='watchlist'>Assistir</Link>
+            </li>
+            <li>
+              <Link to='favorites'>Favoritos</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
   );
 };
