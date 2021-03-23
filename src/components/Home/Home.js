c50f82b8ca9f2a29ad3d653355582a767d2e9c14
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-23 17:13:34 -0300

    content css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,16 @@ import './Home.css';
 export const Home = () => {
   return (
     <div className='container'>
-      <div className='header'></div>
+      <div className='header'>
+        <div className='content-text'>
+          <div className='content-title'>
+            <p>Bem vindo!</p>
+          </div>
+          <div className='content-description'>
+            <p>Explore milhares de filmes e TV Shows</p>
+          </div>
+        </div>
+      </div>
       <h1></h1>
     </div>
   );
