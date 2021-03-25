3d4807c6a3c21d96168d7f77121096c305b4d90e
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-25 11:14:11 -0300

    responsive slider config

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -6,37 +6,440 @@ import cover1 from '../../images/cover1.PNG';
 export const SlideShow = () => {
   const settings = {
     centerMode: true,
+    swipeToSlide: true,
     // centerPadding: '100px',
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 6,
-    slidesToScroll: 1,
+    // slidesToScroll: 1,
     variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1713,
+        settings: {
+          slidesToShow: 5,
+          arrows: true,
+          centerMode: true,
+        },
+      },
+      {
+        breakpoint: 1480,
+        settings: {
+          slidesToShow: 3,
+          arrows: true,
+          centerMode: true,
+        },
+      },
+      {
+        breakpoint: 1250,
+        settings: {
+          slidesToShow: 2,
+          arrows: true,
+          centerMode: true,
+        },
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          arrows: false,
+          centerMode: false,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+          centerMode: false,
+        },
+      },
+    ],
   };
   return (
     <div className='slide-show-wrapper'>
-      {/* <Slider {...settings}> */}
-      <div className='image-card'>
-        <img src={cover1} style={{ width: 160 }} alt='' />
-        <div className='inner-cards-control'>
-          <button className='more-informations'>
-            <i class='far fa-plus-square'></i>
-          </button>
-          <div className='buttons-wrapper'>
-            <button className='add-mylist'>
-              <i class='fas fa-folder-plus'></i>
+      <Slider {...settings}>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
             </button>
-            <button className='add-watchlist'>
-              <i class='far fa-eye'></i>
-            </button>
-            <button className='add-favorites'>
-              <i class='far fa-star'></i>
-            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      {/* </Slider> */}
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='image-card'>
+          <img src={cover1} style={{ width: 160 }} alt='' />
+          <div className='inner-cards-control'>
+            <button className='more-informations'>
+              <i class='far fa-plus-square'></i>
+            </button>
+            <div className='buttons-wrapper'>
+              <button className='add-mylist'>
+                <i class='fas fa-folder-plus'></i>
+              </button>
+              <button className='add-watchlist'>
+                <i class='far fa-eye'></i>
+              </button>
+              <button className='add-favorites'>
+                <i class='far fa-star'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </Slider>
     </div>
   );
 };
