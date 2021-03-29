f7a6fccecad79ef6c1f81582ac89dfa68ebe093e
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-29 14:23:06 -0300

    updating api call in popularMovies

--- src/components/Popular/Popular.js
+++ src/components/Popular/Popular.js
@@ -1,26 +1,23 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { GlobalContext } from '../../context/GlobalState';
 import SlideShow from '../SlideShow/SlideShow';
 
 export const Popular = () => {
   const { addToPopularList, popularMovies } = useContext(GlobalContext);
-  useEffect(
-    () => {
-      fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          if (!data.errors) {
-            addToPopularList(data.results);
-          } else {
-            console.log('erro');
-          }
-        });
-    },
-    // eslint-disable-next-line
-    []
-  );
+  if (popularMovies.length === 0) {
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) {
+          addToPopularList(data.results);
+          console.log('Popular');
+        } else {
+          console.log('erro');
+        }
+      });
+  }
   return (
     <>
       <SlideShow movie={popularMovies} />
