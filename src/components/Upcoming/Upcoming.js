ccfe4fe36e3e8bc0c26c6104978714012f466d0b
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-29 14:30:45 -0300

    upComing api call updated

--- src/components/Upcoming/Upcoming.js
+++ src/components/Upcoming/Upcoming.js
@@ -1,26 +1,23 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { GlobalContext } from '../../context/GlobalState';
 import SlideShow from '../SlideShow/SlideShow';
 
 export const Upcoming = () => {
   const { addToUpComingList, upComingMovies } = useContext(GlobalContext);
-  useEffect(
-    () => {
-      fetch(
-        `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          if (!data.errors) {
-            addToUpComingList(data.results);
-          } else {
-            console.log('erro');
-          }
-        });
-    },
-    // eslint-disable-next-line
-    []
-  );
+  if (upComingMovies.length === 0) {
+    fetch(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.errors) {
+          addToUpComingList(data.results);
+          console.log('entrando');
+        } else {
+          console.log('erro');
+        }
+      });
+  }
   return (
     <>
       <SlideShow movie={upComingMovies} />
