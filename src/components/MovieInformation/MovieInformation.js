994c6158d34dbd50fb86a895b92d4ce9fe813e8c
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-03-30 16:09:00 -0300

    container and header css style

--- src/components/MovieInformation/MovieInformation.js
+++ src/components/MovieInformation/MovieInformation.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import cover1 from '../../images/cover1.PNG';
+import './MovieInformation.css';
 
 const MovieInformation = () => {
   const location = useLocation();
   console.log(location.state.params);
   return (
     <>
-      <h1>MOVIE INFORMATION</h1>
+      <div className='container-movie-information'>
+        <div className='header-content-container'>
+          <div className='header-content'>
+            <img src={cover1} alt='' />
+          </div>
+        </div>
+        <h1>MOVIE INFORMATION</h1>
+      </div>
     </>
   );
 };
