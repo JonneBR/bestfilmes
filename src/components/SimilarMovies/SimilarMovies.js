1d7778504d41f23dae9b7bb3a5e680db3be65077
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-04-02 12:27:01 -0300

    similar movies working

--- src/components/SimilarMovies/SimilarMovies.js
+++ src/components/SimilarMovies/SimilarMovies.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
-
-// https://api.themoviedb.org/3/movie/{id}/similar?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1
+import noPosterAvailable from '../../images/noPosterAvailable.jpg';
 
 const SimilarMovies = ({ id }) => {
-  console.log(id);
   const [similarMovies, setSimilarMovies] = useState([]);
 
   useEffect(() => {
@@ -19,8 +17,50 @@ const SimilarMovies = ({ id }) => {
         }
       });
   }, []);
-  console.log('teste', similarMovies);
-  return <></>;
+  return (
+    <>
+      <div className='search-movie-result-container'>
+        <div className='grid-wrapper'>
+          {similarMovies.map((movie) => {
+            return (
+              <div key={movie.id} className='image-card'>
+                {movie.poster_path !== null ? (
+                  <img
+                    src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+                    style={{ width: 160 }}
+                    alt=''
+                  />
+                ) : (
+                  <img src={noPosterAvailable} style={{ width: 160 }} alt='' />
+                )}
+
+                <div className='inner-cards-control'>
+                  <button
+                    className='more-informations'
+                    // onClick={() => handleMovieInformation(movie.id)}
+                  >
+                    <i className='far fa-plus-square'></i>
+                  </button>
+
+                  <div className='buttons-wrapper'>
+                    <button className='add-mylist'>
+                      <i className='fas fa-folder-plus'></i>
+                    </button>
+                    <button className='add-watchlist'>
+                      <i className='far fa-eye'></i>
+                    </button>
+                    <button className='add-favorites'>
+                      <i className='far fa-star'></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SimilarMovies;
