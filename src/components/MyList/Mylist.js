2eab96960af3c8ea531ba32c9574717a94f9f6d8
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-04-07 11:59:56 -0300

    mylist header css

--- src/components/MyList/Mylist.js
+++ src/components/MyList/Mylist.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Mylist.css';
 
 export const Mylist = () => {
   return (
     <>
-      <h1>MY LIST</h1>
+      <div className='container'>
+        <div className='header'>
+          <div className='main-image-mylist'>
+            <div className='content-text'>
+              <div className='content-title'>
+                <p>Bem vindo!</p>
+              </div>
+              <div className='content-description'>
+                <p>
+                  Explore <strong>milhares</strong> de filmes e TV Shows
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
