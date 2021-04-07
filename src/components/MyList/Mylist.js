b5e45c565086df6e790cb30eda291f5a6ae832ed
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-04-07 12:45:07 -0300

    mylist movies configured

--- src/components/MyList/Mylist.js
+++ src/components/MyList/Mylist.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Mylist.css';
+import noPosterAvailable from '../../images/noPosterAvailable.jpg';
+import { GlobalContext } from '../../context/GlobalState';
+import ButtonControls from '../ButtonControls/ButtonControls';
 
 export const Mylist = () => {
+  const { mylistClickedButton } = useContext(GlobalContext);
   return (
     <>
       <div className='container'>
@@ -13,6 +17,35 @@ export const Mylist = () => {
             <h1>Minha lista</h1>
           </div>
         </div>
+        {mylistClickedButton.length > 0 ? (
+          <div className='search-movie-result-container'>
+            <div className='grid-wrapper'>
+              {mylistClickedButton.map((movie) => {
+                return (
+                  <div key={movie.id} className='image-card'>
+                    {movie.poster_path !== null ? (
+                      <img
+                        src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+                        style={{ width: 160 }}
+                        alt=''
+                      />
+                    ) : (
+                      <img
+                        src={noPosterAvailable}
+                        style={{ width: 160 }}
+                        alt=''
+                      />
+                    )}
+
+                    <ButtonControls movie={movie} />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          console.log('NÃO HÁ FILMES PARA EXIBIR')
+        )}
       </div>
     </>
   );
