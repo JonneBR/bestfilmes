2145a2835989af6e77806ba5218ca494f2ed65ee
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-04-07 10:13:58 -0300

    search movie results component refactoring

--- src/components/SearchMovieResult/SearchMovieResult.js
+++ src/components/SearchMovieResult/SearchMovieResult.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import noPosterAvailable from '../../images/noPosterAvailable.jpg';
+import ButtonControls from '../ButtonControls/ButtonControls';
 import './SearchMovieResult.css';
 
 const SearchMovieResult = ({ movies }) => {
-  const history = useHistory();
-
-  //Routing and passing parameters to MovieInformation
-  const handleMovieInformation = (id) => {
-    const movieClickedInfo = movies.find(
-      (movieSelected) => movieSelected.id === id
-    );
-    history.push('/MovieInformation', {
-      params: movieClickedInfo,
-      movie: movies,
-    });
-  };
-
   return (
     <>
       <div className='search-movie-result-container'>
@@ -34,26 +21,7 @@ const SearchMovieResult = ({ movies }) => {
                   <img src={noPosterAvailable} style={{ width: 160 }} alt='' />
                 )}
 
-                <div className='inner-cards-control'>
-                  <button
-                    className='more-informations'
-                    onClick={() => handleMovieInformation(movie.id)}
-                  >
-                    <i className='far fa-plus-square'></i>
-                  </button>
-
-                  <div className='buttons-wrapper'>
-                    <button className='add-mylist'>
-                      <i className='fas fa-folder-plus'></i>
-                    </button>
-                    <button className='add-watchlist'>
-                      <i className='far fa-eye'></i>
-                    </button>
-                    <button className='add-favorites'>
-                      <i className='far fa-star'></i>
-                    </button>
-                  </div>
-                </div>
+                <ButtonControls movie={movie} />
               </div>
             );
           })}
